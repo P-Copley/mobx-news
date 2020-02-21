3265b5547325dce9aef9067372eb3f18f6e4fada
Paul Copley <paul@northcoders.com>
2020-02-21 14:16:54 +0000

    annotate example with explanation

--- src/models/articles.js
+++ src/models/articles.js
@@ -1,6 +1,13 @@
 import { types } from 'mobx-state-tree';
 import Article from './article';
 
+/*
+An example of how a strongly typed store for articles can be implemented using mobx-state-tree
+Not used in the app but here as an example that would work.
+All updates to the store are performed through actions. (This can be turned off to allow any mutations)
+Derivations can be calculated and tested using views.
+*/
+
 const Articles = types
   .model('Articles', {
     articles: types.array(Article)
