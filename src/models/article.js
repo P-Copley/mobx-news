94933258bf6c7071318c97ec282a751118574bbc
Paul Copley <paul@northcoders.com>
2020-02-21 14:12:59 +0000

    add mobx-state-tree models for articles as examples

---
+++ src/models/article.js
@@ -0,0 +1,24 @@
+import { types } from 'mobx-state-tree';
+
+const Article = types
+  .model('Article', {
+    title: types.string,
+    votes: types.number,
+    article_id: types.number,
+    created_at: types.string,
+    author: types.string,
+    topic: types.string,
+    comment_count: types.string
+  })
+  .actions(self => ({
+    vote(increment) {
+      self.votes += increment;
+    }
+  }))
+  .views(self => ({
+    getBodyPreview() {
+      return `${self.body.slice(0, 100)}...`;
+    }
+  }));
+
+export default Article;
